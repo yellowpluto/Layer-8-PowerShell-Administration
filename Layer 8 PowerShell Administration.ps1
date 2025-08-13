424126c8c9ec7b3f36882541c336325e28db4f31
yellowpluto <yellowpluto31569@gmail.com>
2025-08-13 01:05:38 -0700

    change

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -31,11 +31,52 @@ switch ($num){
 		Write-Host "Choose an OU"
 		$choose = Read-Host
 		$result = $listArray[$choose - 1]
-		Get-ADUser -Filter * -SearchBase $result | Select-Object -ExpandProperty Name
+		$users = @(Get-ADUser -Filter * -SearchBase $result | Select-Object -ExpandProperty Name)
+		$numList = 1
+		foreach($user in $users){
+			"(" + $numList.ToString() + ")" + $user
+			$numList++
+			$users += $users
+		}
+		
+		Write-Host "Choose a user"
+		$choose = Read-Host
+		$result = $users[$choose - 1]
+		$if = Read-Host "Change password on next logon? (Y/N)"
+		if($if -eq "Y"){
+			Set-ADUser -Identity $result -ChangePasswordAtLogon $true
+			Set-ADAccountPassword -Identity $result -Reset
+		}ElseIf($if -eq "N"){
+			Set-ADUser -Identity $result -ChangePasswordAtLogon $false
+			Set-ADAccountPassword -Identity $result -Reset
+		}Else{
+			Write-Host -ForegroundColor Red "Incorrect value proceeding to password reset"
+			Set-ADAccountPassword -Identity $result -Reset
+		}
+			
+		break
 		
 	}
-
+	
+	3 {
+	
+		$condition = Read-Host "GPU and Restart Machines (Y/N)"
+			if($condition -eq "Y"){
+				Invoke-GPUpdate C-01 -Force -Boot
+			}ElseIf($condition -eq "N"){
+				Invoke-GPUpdate C-01 -Force
+			}Else{
+			Write-Host -ForegroundColor Red "Incorrect value proceeding to regular update"
+			Invoke-GPUpdate C-01 -Force
+		}
+		break
+	}
 		
+	4 {
+	
+		Restart-Computer C-01 -Force -Confirm
+		break
+	}
 	
 	
 	
