8785cfc1c438273b29bea6adda9a00c0fcc31aec
yellowpluto <yellowpluto31569@gmail.com>
2025-08-15 23:44:56 -0700

    change

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -164,9 +164,19 @@ switch ($num){
 	
 	7 {
 	
-		Get-ADUser -Filter * | Select-Object -ExpandProperty SamAccountName | ForEach-Object {Set-ADUser -Identity $_ -EMailAddress "$_@AnimeHealth.net"}
+		$listArray = @()
+		$numList = 1
+		$OUArr = Get-ADOrganizationalUnit -Filter * | Select-Object -ExpandProperty DistinguishedName | Sort-Object -desc
+		foreach($OU in $OUArr){
+			"(" + $numList.ToString() + ")" + $OU
+			$numList++
+			$listArray += $OU
+		}
+		$choose = Read-Host "Choose OU"
+		$result = $listArray[$choose - 1]
+		Write-Host -ForegroundColor Yellow "$result CHOSEN"
+		Get-ADUser -Filter * -SearchBase "$result" | Select-Object -ExpandProperty SamAccountName | ForEach-Object {Set-ADUser -Identity $_ -EMailAddress "$_@AnimeHealth.net"}
 		break
-	
 	}
 	
 	
