8aa40c217f2fe58c4852d9501794fef84cee5791
yellowpluto <yellowpluto31569@gmail.com>
2025-08-27 14:09:15 -0700

    change

--- Scripts/3a.ps1
+++ Scripts/3a.ps1
@@ -1,11 +1,11 @@
 $condition = Read-Host "GPU and Restart Machines (Y/N)"
-			if ($condition -eq "Y") {
+if ($condition -eq "Y") {
 				Invoke-GPUpdate NPOD1, NPOD2, NPOD3, NPOD4, WPOD1, WPOD2, WPOD3, WPOD4, LPOD1, LPOD2, LPOD3, LPOD4 -Force -Boot
-			}
-			ElseIf ($condition -eq "N") {
+}
+ElseIf ($condition -eq "N") {
 				Invoke-GPUpdate NPOD1, NPOD2, NPOD3, NPOD4, WPOD1, WPOD2, WPOD3, WPOD4, LPOD1, LPOD2, LPOD3, LPOD4 -Force
-			}
-			Else {
+}
+Else {
 				Write-Host -ForegroundColor Red "Incorrect value proceeding to regular update"
 				Invoke-GPUpdate NPOD1, NPOD2, NPOD3, NPOD4, WPOD1, WPOD2, WPOD3, WPOD4, LPOD1, LPOD2, LPOD3, LPOD4 -Force
-			}
+}
--- Scripts/5a.ps1
+++ Scripts/5a.ps1
@@ -1,21 +1,21 @@
 $listArray = @()
-			$numList = 1
-			$5a = Get-ADOrganizationalUnit -Filter * | Select-Object -ExpandProperty DistinguishedName | Sort-Object -desc
-			foreach ($5 in $5a) {
+$numList = 1
+$5a = Get-ADOrganizationalUnit -Filter * | Select-Object -ExpandProperty DistinguishedName | Sort-Object -desc
+foreach ($5 in $5a) {
 				"(" + $numList.ToString() + ")" + $5
 				$numList++
 				$listArray += $5
-			}
+}
 		
-			Write-Host "Choose an OU"
-			$choose = Read-Host
-			$result = $listArray[$choose - 1]
-			$users = @(Get-ADUser -Filter * -SearchBase $result | Select-Object -ExpandProperty SamAccountName | Sort-Object -desc)
-			Write-Host -ForegroundColor Yellow "How powershell reads dates:'6/9/2069'"
-			[string]$year = Read-Host "Enter year"
-			[string]$month = Read-Host "Enter month (ADD '/' @ END)"
-			[string]$day = Read-Host "Enter day(ADD '/' @ END)"
-			[string]$date = $month + $day + $year
-			foreach ($user in $users) {
+Write-Host "Choose an OU"
+$choose = Read-Host
+$result = $listArray[$choose - 1]
+$users = @(Get-ADUser -Filter * -SearchBase $result | Select-Object -ExpandProperty SamAccountName | Sort-Object -desc)
+Write-Host -ForegroundColor Yellow "How powershell reads dates:'6/9/2069'"
+[string]$year = Read-Host "Enter year"
+[string]$month = Read-Host "Enter month (ADD '/' @ END)"
+[string]$day = Read-Host "Enter day(ADD '/' @ END)"
+[string]$date = $month + $day + $year
+foreach ($user in $users) {
 				Set-ADAccountExpiration -Credential $Global:credential -Identity $user -DateTime $date
-			}
+}
