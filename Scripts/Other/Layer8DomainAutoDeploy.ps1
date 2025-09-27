9ea68420dd83caf1770e9a68cfc898ae34e1d936
yellowpluto <yellowpluto31569@gmail.com>
2025-09-26 23:37:52 -0700

    stuff

--- Scripts/Other/Layer8DomainAutoDeploy.ps1
+++ Scripts/Other/Layer8DomainAutoDeploy.ps1
@@ -1,1 +1,2 @@
-Install-Module ActiveDirectory
+Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName "CSUNLayer8.com" -SafeModeAdministratorPassword (Read-Host -Prompt "DSRM Password" -AsSecureString) -InstallDns
