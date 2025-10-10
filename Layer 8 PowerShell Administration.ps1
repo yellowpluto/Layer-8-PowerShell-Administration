3ea6249a677d2b3f123b95998ce5f20a7aa4b6e8
yellowpluto <yellowpluto31569@gmail.com>
2025-10-09 21:45:03 -0700

    101b?

--- Layer 8 PowerShell Administration.ps1
+++ Layer 8 PowerShell Administration.ps1
@@ -815,6 +815,59 @@ function Get-InventoryDomainLocal {
 	}	
 }
 
+#101b
+function Get-InventoryDomainRemote {
+	
+	$newPSSessions = Get-Content "$PSScriptRoot\PSSessions.txt"
+	Invoke-Command -ScriptBlock {
+	
+		Write-Host -ForegroundColor Yellow "Script runs in background."
+	
+		Start-Job -Name "Get-InventoryDomainLocal" -ScriptBlock {
+	
+			$t = get-date -format yyyymmdd_HHmm
+			$fileName = "invFile"
+		
+			if ((Test-Path -Path "C:\output\invFile.txt") -eq ($true)) {
+				Clear-Content -Path "C:\output\invFile.txt"
+			}
+			
+			$computers = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+		
+			foreach ($computer in $computers) {
+				if ($computer -eq (Get-ComputerInfo | select-Object -ExpandProperty CsName)) {
+				
+					$ipAddress = Get-NetIPAddress | Select-Object -ExpandProperty IPv4Address | Where-Object { $_ -notlike "127.*" }
+					$macAddress = Get-NetAdapter | select-Object -ExpandProperty MacAddress
+					$osName = Get-ComputerInfo | Select-Object -ExpandProperty osname
+						
+				}
+				else {
+					try {
+						$ipAddress = Invoke-Command -ComputerName $computer -ScriptBlock { Get-NetIPAddress | Select-Object -ExpandProperty IPv4Address | Where-Object { $_ -notlike "127.*" } } -ErrorAction Stop 
+						$macAddress = Invoke-Command -ComputerName $computer -ScriptBlock { Get-NetAdapter | select-Object -ExpandProperty MacAddress } -ErrorAction Stop 
+						$osName = Invoke-Command -ComputerName $computer -ScriptBlock { Get-ComputerInfo | Select-Object -ExpandProperty osname } -ErrorAction Stop 
+					}
+					catch {
+						Write-Host -ForegroundColor Red "Something went wrong...is WinRM configured correctly on all machines/are the machines online?"	
+						continue				
+					}
+				}
+			
+				$output = @($computer + ": " + $macAddress + ", " + $ipAddress + ", " + $osName)
+				$dynamicFile += $output
+				# Writing current user's PW to the PW file and loop to get the next user.
+				$output | Out-File -FilePath "C:\output\$fileName.txt" -Append
+			}
+
+			# Writing out the dynamic file	
+			$fileName = "invFile_$t"
+			$dynamicFile | Out-File -FilePath "C:\output\$fileName.txt"
+			Invoke-Item -Path "C:\output\"
+		}
+	} -ComputerName $newPSSessions
+}
+
 #102a
 function Set-RandomADPasswords {
 	#add random characters
@@ -1169,6 +1222,13 @@ while ($start -eq $true) {
 			Get-InventoryDomainLocal
 			break
 	
+		}
+
+		101b {
+
+			Get-InventoryDomainRemote
+			break
+
 		}
 
 		102a {
